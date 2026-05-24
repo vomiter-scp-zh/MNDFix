292ae6eb33d760352a7f13401638c4f189f6dbdb
vomiter-scp-zh <97169118+vomiter-scp-zh@users.noreply.github.com>
2026-05-24 08:10:35 +0800

    for ALEX'S CAVE DELIGHT AND ALEX'S MOB DELIGHT

---
+++ src/main/resources/coremods/alexscavesdelight_shepherds_pie_to_feast.js
@@ -0,0 +1,64 @@
+var ASMAPI = Java.type("net.minecraftforge.coremod.api.ASMAPI");
+var Opcodes = Java.type("org.objectweb.asm.Opcodes");
+
+var TARGET_CLASS = "net.fixerlink.alexscavesdelight.block.ModBlocks";
+
+var OLD_OWNER = "vectorwing/farmersdelight/common/block/ShepherdsPieBlock";
+var NEW_OWNER = "vectorwing/farmersdelight/common/block/FeastBlock";
+
+// Both ShepherdsPieBlock and FeastBlock use this constructor shape in the ModBlocks:
+// new X(BlockBehaviour.Properties, Supplier<Item>, boolean)
+var CTOR_DESC =
+    "(Lnet/minecraft/world/level/block/state/BlockBehaviour$Properties;Ljava/util/function/Supplier;Z)V";
+
+function initializeCoreMod() {
+    return {
+        "alexscavesdelight_shepherds_pie_to_feast": {
+            "target": {
+                "type": "CLASS",
+                "name": TARGET_CLASS
+            },
+            "transformer": function(classNode) {
+                var changed = 0;
+
+                for (var i = 0; i < classNode.methods.size(); i++) {
+                    var method = classNode.methods.get(i);
+                    var instructions = method.instructions;
+
+                    for (var instruction = instructions.getFirst(); instruction != null; instruction = instruction.getNext()) {
+                        // Replace: NEW vectorwing/farmersdelight/common/block/ShepherdsPieBlock
+                        if (instruction.getOpcode && instruction.getOpcode() === Opcodes.NEW) {
+                            if (instruction.desc === OLD_OWNER) {
+                                //NEW: desc is class internal name
+                                instruction.desc = NEW_OWNER;
+                                changed++;
+                            }
+                        }
+
+                        // Replace:
+                        // INVOKESPECIAL vectorwing/farmersdelight/common/block/ShepherdsPieBlock.<init>(Properties, Supplier, boolean)V
+                        if (instruction.getOpcode && instruction.getOpcode() === Opcodes.INVOKESPECIAL) {
+                            if (
+                                instruction.owner === OLD_OWNER &&
+                                instruction.name === "<init>" &&
+                                instruction.desc === CTOR_DESC
+                            ) {
+                                instruction.owner = NEW_OWNER;
+                                changed++;
+                            }
+                        }
+                    }
+                }
+
+                ASMAPI.log(
+                    "INFO",
+                    "[MND Fix] Retargeted " + changed +
+                    " ShepherdsPieBlock constructor references to FeastBlock in " +
+                    TARGET_CLASS
+                );
+
+                return classNode;
+            }
+        }
+    };
+}
